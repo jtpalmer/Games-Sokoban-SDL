5d5c554b5c5077eda2061680d1cf34caeb02d766
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2011-01-12 20:59:43 -0500

    Movement, event handling

--- bin/sokoban.pl
+++ bin/sokoban.pl
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 
 use SDL;
+use SDL::Event;
 use SDLx::App;
 use SDLx::Surface;
 use SDLx::Sprite;
@@ -128,6 +129,71 @@ sub init_goal {
         };
 }
 
+sub move_player {
+    my ($direction) = @_;
+
+    my ( $dx, $dy ) = ( 0, 0 );
+    $dx = -1 if $direction eq 'west';
+    $dx = 1  if $direction eq 'east';
+    $dy = -1 if $direction eq 'north';
+    $dy = 1  if $direction eq 'south';
+
+    my $x = $PLAYER{x} + $dx;
+    my $y = $PLAYER{y} + $dy;
+
+    if ( wall_at( $x, $y ) ) {
+        return;
+    }
+
+    if ( my $box = box_at( $x, $y ) ) {
+        my $box_x = $x + $dx;
+        my $box_y = $y + $dy;
+        if ( wall_at( $box_x, $box_y ) || box_at( $box_x, $box_y ) ) {
+            return;
+        }
+        else {
+            $box->{x} = $box_x;
+            $box->{y} = $box_y;
+            $box->{sprite}->x( $box_x * $SIZE );
+            $box->{sprite}->y( $box_y * $SIZE );
+        }
+    }
+
+    $PLAYER{x} = $x;
+    $PLAYER{y} = $y;
+    $PLAYER{sprite}->x( $x * $SIZE );
+    $PLAYER{sprite}->y( $y * $SIZE );
+}
+
+sub wall_at {
+    my ( $x, $y ) = @_;
+
+    foreach my $wall (@WALLS) {
+        return $wall if $wall->{x} == $x && $wall->{y} == $y;
+    }
+    return;
+}
+
+sub box_at {
+    my ( $x, $y ) = @_;
+
+    foreach my $box (@BOXES) {
+        return $box if $box->{x} == $x && $box->{y} == $y;
+    }
+    return;
+}
+
+sub handle_event {
+    my ($event) = @_;
+
+    if ( $event->type == SDL_KEYDOWN ) {
+        move_player('west')  if $event->key_sym == SDLK_LEFT;
+        move_player('east')  if $event->key_sym == SDLK_RIGHT;
+        move_player('north') if $event->key_sym == SDLK_UP;
+        move_player('south') if $event->key_sym == SDLK_DOWN;
+    }
+}
+
 sub handle_show {
     foreach my $wall (@WALLS) { $wall->{sprite}->draw($APP); }
     foreach my $goal (@GOALS) { $goal->{sprite}->draw($APP); }
@@ -136,6 +202,7 @@ sub handle_show {
     $APP->update();
 }
 
+$APP->add_event_handler( \&handle_event );
 $APP->add_show_handler( \&handle_show );
 init_level( Games::Sokoban->new_from_file( $SHARE->file('level1.sok') ) );
 $APP->run();
